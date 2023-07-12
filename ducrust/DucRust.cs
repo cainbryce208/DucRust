87437289bef5b76c81e0f3c6c64372202a265cfa
cainbryce208 <cainbryce208@gmail.com>
2023-07-12 13:07:06 -0700

    First Basic Plugin Commit

---
+++ ducrust/DucRust.cs
@@ -0,0 +1,19 @@
+using Oxide.Core;
+using Oxide.Core.Plugins;
+using Rust;
+
+namespace Oxide.Plugins
+{
+    [Info("DucRust", "cainb208", "0.0.1")]
+    class DucRust : RustPlugin
+    {
+        void Init()
+        {
+    
+        }
+        void OnPlayerConnected(BasePlayer player)
+        {
+            
+        }
+    }
+}
